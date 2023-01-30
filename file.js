a97ee25496745c773be800b60ac8bc3d2fe9e717
Sh7909 <118052273+Sh7909@users.noreply.github.com>
2023-01-30 19:26:22 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,11 @@
+    function login(){
+       var x=document.getElementById("m1");
+       if (x.style.display === "none") {
+       x.style.display = "block";
+     } else {
+       x.style.display = "none";
+     }
+
+    }
+
+
